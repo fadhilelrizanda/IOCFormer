03dc5843c1f98107456c4e6bc45093bf55334f13
fadhilelrizanda <fadhilelrizanda@gmail.com>
2026-02-04 18:05:13 +0700

    patch

--- train_distributed.py
+++ train_distributed.py
@@ -32,7 +32,7 @@ warnings.filterwarnings('ignore')
 setup_seed(args.seed)
 
 # model save path, set this path to where you would like to save model/log
-exp_save='/cluster/work/cvl/guosun/models/cod-counting/'
+exp_save='./training_weights/'
 
 def main(args):
     if args['dataset'] == 'jhu':
